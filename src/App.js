5c2ad68ddd758f93f44bd159da85b1623d4cf96a
mikeejazmines <mikee.jazmines@gmail.com>
2020-07-21 16:12:59 +0800

    Clean structure

--- src/App.js
+++ src/App.js
@@ -3,30 +3,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Test from './components/Test';
+import Calculator from './components/Calculator/Calculator';
 
 class App extends Component {
   render() {
     return (
     <Router>
         <div>
-          <h2>Welcome to React Router Tutorial</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-            <li><Link to={'/about'} className="nav-link">About</Link></li>
-            <li><Link to={'/test'} className="nav-link">Test</Link></li>
-          </ul>
-          </nav>
-          <hr />
+          <h2>React Practices</h2>
+          <Link to={'/'} className="nav-link"> Home </Link>
           <Switch>
               <Route exact path='/' component={Home} />
-              <Route path='/contact' component={Contact} />
-              <Route path='/about' component={About} />
-              <Route path='/test' component={Test} />
+              <Route path='/calculator' component={Calculator} />
           </Switch>
         </div>
       </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,21 @@
-// Home.js
+// This will be where all the list of my mini apps will be found as well as the date they were initially made
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
-  render() {
-    return (
-        <div>
-          <h2>Home</h2>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+	          <h2>Home</h2>
+	          <p>I will be creating different react apps to practice my react </p>
+	          <p>This will not focus on css yet, mostly just functionality </p>
+	          <ul className="navbar-nav mr-auto">
+	            <li><Link to={'/calculator'} className="nav-link">Calculator - July 21, 2020</Link></li>
+	          </ul>
+	        </div>
+        );
+    }
 }
 
 export default Home;
